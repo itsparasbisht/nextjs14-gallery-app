b4454af43a4ab1169afa38588c3b6b4a0e3d5469
itsparasbisht <iamparasbisht@gmail.com>
2024-04-26 20:34:59 +0530

    images schema + db link with ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,19 @@
 import { db } from "@/server/db";
-import { user } from "@/server/db/schema";
 
 export const dynamic = "force-dynamic";
 
-const mockImages = [
-  {
-    id: 1,
-    url: "https://utfs.io/f/ab3e4e6b-9d89-4200-85b4-39c06a5136e9-ym54qo.jpg",
-  },
-  {
-    id: 2,
-    url: "https://utfs.io/f/6c34764c-5a9b-45a8-84f9-9906ade55596-7c32st.at_by_ladyvenom_south_tyrol-231.jpg",
-  },
-  {
-    id: 3,
-    url: "https://utfs.io/f/341d1fbe-16ea-4f79-ae43-f5276150faa2-43ftnp.jpeg",
-  },
-  {
-    id: 4,
-    url: "https://utfs.io/f/15f712bb-0f42-4f3c-bb48-5ca29f276155-258xty.jpg",
-  },
-];
-
 export default async function Home() {
-  const users = await db.query.user.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {users.map((user) => (
-          <h1>{user.name}</h1>
-        ))}
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+        {images.map((image, index) => (
           <div key={index} className="w-48">
             <img src={image.url} />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,12 +1,24 @@
-import { serial, text, timestamp, pgTable } from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
+import {
+  serial,
+  timestamp,
+  pgTable,
+  varchar,
+  index,
+} from "drizzle-orm/pg-core";
 
-export const user = pgTable("user", {
-  id: serial("id").primaryKey(),
-  name: text("name"),
-  email: text("email"),
-  password: text("password"),
-  phone: text("phone"),
-  role: text("role").$type<"admin" | "customer">(),
-  createdAt: timestamp("created_at"),
-  updatedAt: timestamp("updated_at"),
-});
+export const images = pgTable(
+  "image",
+  {
+    id: serial("id").primaryKey(),
+    name: varchar("name", { length: 255 }).notNull(),
+    url: varchar("url", { length: 1024 }).notNull(),
+    createdAt: timestamp("created_at")
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at"),
+  },
+  (table) => ({
+    nameIndex: index("name_idx").on(table.name),
+  })
+);
