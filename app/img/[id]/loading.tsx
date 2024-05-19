2d7a04a30db9d60cf3af76b503a27a2b0e4514a8
itsparasbisht <iamparasbisht@gmail.com>
2024-05-19 20:16:57 +0530

    loading files added with condition

---
+++ app/img/[id]/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="fixed w-screen flex justify-center">
+      <p className="bg-orange-200 p-1 text-black font-medium px-2 rounded-sm">
+        loading...
+      </p>
+    </div>
+  );
+}
--- app/loading.tsx
+++ app/loading.tsx
@@ -1,4 +1,12 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
 export default function Loading() {
+  const pathname = usePathname();
+
+  if (pathname.startsWith("/img")) return null;
+
   return (
     <div className="w-screen h-[600px] text-lg text-gray-400 flex justify-center items-center">
       loading...
